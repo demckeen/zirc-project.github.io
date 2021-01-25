cf7c8d2f26b93e55a6f565ecb66482e9e1a81b5a
zircadia <76924125+zircadia@users.noreply.github.com>
2021-01-25 10:22:35 -0600

    commit

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -1,2 +1,9 @@
 document.getElementById("demo").innerHTML = document.lastModified;
 document.getElementById("year").innerHTML = new Date().getFullYear();
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
