3d20038c035ab267697c1c4a3c7ed3854ed8c41b
zircadia <76924125+zircadia@users.noreply.github.com>
2021-01-29 19:48:51 -0600

    Correct date js

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -1,4 +1,4 @@
-document.getElementById("demo").innerHTML = document.lastModified;
+
 document.getElementById("year").innerHTML = new Date().getFullYear();
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
@@ -7,3 +7,26 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')}
+
+//current day of week
+(function() {
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+    Date.prototype.getMonthName = function() {
+        return months[ this.getMonth() ];
+    };
+    Date.prototype.getDayName = function() {
+        return days[ this.getDay() ];
+    };
+})();
+
+var now = new Date();
+
+var day = now.getDayName();
+var month = now.getMonthName();
+var date = new Date().toLocaleDateString()
+
+
+document.getElementById("dayofweek").innerHTML = day + ", " + now.getDate() + " " + month + " " + now.getFullYear();
