447ada82f3f8de2d7395f77eab3f7b353e57b5e5
zircadia <76924125+zircadia@users.noreply.github.com>
2021-03-08 09:44:07 -0600

    apitest

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,16 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=cbf6f180a0de7965e7eda02d85df55c9";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    document.getElementById('current-temp').textContent = ((jsObject.main.temp - 273.15) * 9/5 + 32).toFixed(2);
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+
+
+  });
