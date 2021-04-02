8a45621d059b34dd9753650c71afe8f179447304
zircadia <76924125+zircadia@users.noreply.github.com>
2021-04-02 14:08:06 -0500

    cityinfo page content

--- final/js/directory.js
+++ final/js/directory.js
@@ -6,3 +6,4 @@ const mainnav = document.querySelector(".navigation");
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
