abf5e22794dd09b0bfef1ddfab55927d9e6acf71
zircadia <76924125+zircadia@users.noreply.github.com>
2021-04-06 16:01:43 -0500

    javascript updates

--- final/js/directory.js
+++ final/js/directory.js
@@ -49,3 +49,21 @@ fetch(requestURL)
         card.appendChild(web);
         
         document.querySelector('div.cards').appendChild(card);}});
+
+        if('IntersectionObserver' in window) {
+          const observer = new IntersectionObserver((items, observer) => {
+            items.forEach((item) => {
+              if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+              }
+            });
+          });
+          imagesToLoad.forEach((img) => {
+            observer.observe(img);
+          });
+        } else {
+          imagesToLoad.forEach((img) => {
+            loadImages(img);
+          });
+        }
